425218dd972e689d0821c310cbeae90f5dcfe0ad
Chris Simonds <32778284+chrissimonds@users.noreply.github.com>
2024-09-30 23:10:40 -0500

    Update main.py
    Added localtime readout after WiFi connects

--- main.py
+++ main.py
@@ -246,7 +246,7 @@ reading = sensor_temp.read_u16() * conversion_factor
 temperature = 27 - (reading - 0.706) / 0.001721
 fahrenheit = temperature * (9 / 5) + 32
 print("Temperature = " + str(temperature) + " C  " + str(fahrenheit) + " F")
-
+print ('Time is : ',time.localtime())
 print("----------")
 
 # Onboard LED Setup
